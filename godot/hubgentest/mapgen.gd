b8032bc1e67d161bcc1f02959ceaec0b1802991a
SkotDude <159684904+SkotDude@users.noreply.github.com>
2024-02-14 00:04:15 +1000

    Add files via upload

--- godot/hubgentest/mapgen.gd
+++ godot/hubgentest/mapgen.gd
@@ -1,92 +1,142 @@
 extends Node2D
 
 @onready var tileMap = $TileMap
-var forestTile = Vector2i(1,1)
-var doorWayTile = Vector2i(6,1)
+
+var gatewayScenes = [preload("res://gateways/gateway0.tscn")]
+var playerScene = preload("res://player.tscn")
+
+var forestTile = Vector2i(0,0)
+var wallTile = Vector2i(1,0)
 
 var neighFour = [Vector2i(1,0), Vector2i(0,1), Vector2i(-1,0), Vector2i(0,-1)]
+var neighEight = [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1), Vector2i(-1, 1), 
+				Vector2i(-1, 0), Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1)]
+
+@export var gatewayAmount = 15
+@export var gridScaleFactor = 0.75
+@export var gridCellSize = 256
+@export var extraPathwayFactor = 0.1
+
+#these two variables affect the avoidance process... changing them can cause the map to load
+#very fast, but have unreachable areas, or take a very long/infinite time to load
+#
+#The avoidance process can be monitored in the debugging output, if it stops printing out
+#"avoidance running" for a long time, it's probably stuck and won't recover
+@export var entryWayDist = 80	#default 80
+@export var pathwayAvoidanceDist = 11	#default 11
+
+var is_mapPreview = true
+
+var gateways = []
 
 func _ready():
-	#Right now I'm just generating square areas for the doorway rooms, but these could be hand crafted later.
-	var roomAmount = 20
-	# the rooms array is a just a list of coordinates defining the centre of all the rooms.
-	var rooms = []
-	while rooms.size() < roomAmount:
-		#pick a random spot in a range of (-100 to 100, -100 to 100)
-		#-100 to 100 is just an arbitrary number right now, but this would ultimately decide the maximum size of the forest
-		# the actual width/height of the map in pixels is more, this is in terms of tiles, in this
-		#example the tile size of the tilemap is 16x16, so the actual pixel dimensions would be
-		#from -1600 to 1600
-		var roomCentre = Vector2i(randi_range(-100, 100),randi_range(-100, 100))
-		#need to check if the random spot is too close to a preplaced room, this number will need to be fine tuned
-		#if there's not enough space to place all the rooms then it's gonna hang (bad)
-		var isValid = true
-		for i in rooms:
-			#if it's closer than 20 tiles to another room then we're just not gonna do anything, and start the while loop over
-			#we need to cast to Vector2 to work out this distance between points, for some reason you cant do that with Vector2i
-			if Vector2(roomCentre).distance_to(Vector2(i)) < 20:
-				isValid = false
-				break
-		#if it gets through all the rooms and is valid, add it to the room list.
-		if isValid:
-			rooms.append(roomCentre)
-		
-	#now we should have the coordinates for as many rooms as we defined with roomAmount
-	#first, connect them all together with a minimal spanning tree, I'm using the 
-	#A* object because it's a built in graph data structure, but we're not really doing any
-	#navigation, just using it to store nodes(rooms) and generate the edges (connections)
 	
-	var pathWays = find_mst(rooms.duplicate())
-	#this function returns an array of vector2 pairs, each pair being the start point and end point of a pathway.
+# this was a bit of a hacky way to block the player in, but it's waaaay too slow to even be functional
+#	for x in range(-(gatewayAmount*gridScaleFactor)/2*gridCellSize, (gatewayAmount*gridScaleFactor)/2*gridCellSize):
+#		for y in range(-(gatewayAmount*gridScaleFactor)/2*gridCellSize, (gatewayAmount*gridScaleFactor)/2*gridCellSize):
+#			tileMap.set_cell(0,Vector2i(x,y),0,wallTile)
+	
+	var gatewayCoords = []
+	var connections = []
+	var pathWays = []
+	var pathWayCoords = []
+	
+	gateways = generate_gateways(gatewayAmount)
+	
+	for i in gateways:
+		add_child(i)
+		gatewayCoords.append(tileMap.local_to_map(i.position))
+	
+	
+	var mstEdges = find_mst(gatewayCoords.duplicate())
+	for i in mstEdges:
+		if !connections.has(i) and !connections.has([i[1],i[0]]):
+			connections.append(i)
+	
+	var extraEdges = find_tri_edges(gatewayCoords.duplicate())
+	
+	for i in extraEdges:
+		if !connections.has(i) and !connections.has([i[1],i[0]]):
+			if randf() < extraPathwayFactor:
+				connections.append(i)
 
-	#now we can add some extra pathways to make it less linear. I'm gonna use delaunay triangulation, and just
-	# grab a couple of extra pathways from that
-	
-	var roomsTriangulated = Geometry2D.triangulate_delaunay(rooms)
-	#this returns an array of triangles, 0,1,2 make one triangle, 3,4,5 makes the next etc.
-	#need to tidy this up into an array of vector2 pairs like the pathWays array
-	var roomEdges = []
-	for i in range(0, roomsTriangulated.size(), 3):
-		roomEdges.append([rooms[roomsTriangulated[i]],
-							rooms[roomsTriangulated[i + 1]]])
-		roomEdges.append([rooms[roomsTriangulated[i + 1]],
-							rooms[roomsTriangulated[i + 2]]])
-		roomEdges.append([rooms[roomsTriangulated[i + 2]],
-							rooms[roomsTriangulated[i]]])
-	
-	#now we have another array of vector2 pairs, we'll just randomly pick a few from them and add
-	#the too the pathWays array
-	
-	for i in roomEdges:
-		#this number here is the percentage, 0.15 means that on average 15% of the pathways will be picked.
-		if randf() < 0.15:
-			#we'll also check to see if this pathway is already in the array
-			if !pathWays.has(i):
-				pathWays.append(i)
-	
-	#this is a little messy right now, as there are duplicates pathways, this can be cleaned up though
-	
-	#so now we have an array that defines the rooms, and an array that defines the connections
-	#we just need to feed this information into the tile map, right now we're just adding the
-	#walkable ground tiles, adding the wall tiles need to be discussed
-	
-	#first i'm gonna add the pathways, then put the rooms in
-	#the pathways are just a start point and end point, we just connect them in straight lines,
-	#but I do have a random walker method to make windy paths
+	for i in connections:
+		var gatewayIndex0 = gatewayCoords.find(Vector2i(i[0]))
+		var gatewayIndex1 = gatewayCoords.find(Vector2i(i[1]))
+		pathWays.append([gateways[gatewayIndex0], gateways[gatewayIndex1]])
 	
 	for i in pathWays:
-		#the walker method returns a list of points that connect the start to the end of the pathway
-		var points = walker(i)
+		var point1 = Vector2.ZERO
+		var point2 = Vector2.ZERO
+		var startConnector = []
+		var endConnector = []
+		
+		var minDist = INF
+		var closest = Vector2.ZERO
+		for j in i[0].get_node("entry_points").get_children():
+			if j.global_position.distance_to(i[1].global_position) < minDist:
+				minDist = j.global_position.distance_to(i[1].global_position)
+				closest = j.global_position + (Vector2.RIGHT.rotated(i[0].global_position.angle_to_point(j.global_position))*entryWayDist)
+				startConnector = [tileMap.local_to_map(j.global_position), tileMap.local_to_map(closest)]
+		point1 = closest
+		
+		minDist = INF
+		closest = Vector2.ZERO
+		for j in i[1].get_node("entry_points").get_children():
+			if j.global_position.distance_to(i[0].global_position) < minDist:
+				minDist = j.global_position.distance_to(i[0].global_position)
+				closest = j.global_position + (Vector2.RIGHT.rotated(i[1].global_position.angle_to_point(j.global_position))*entryWayDist)
+				endConnector = [tileMap.local_to_map(closest), tileMap.local_to_map(j.global_position)]
+		point2 = closest
+		
+		pathWayCoords.append(startConnector)
+		pathWayCoords.append([tileMap.local_to_map(point1), tileMap.local_to_map(point2)])
+		pathWayCoords.append(endConnector)
+	
+	#this is not great, just to block the player in
+	for i in gatewayCoords:
+		for x in range(-8, 8):
+			for y in range(-8,8):
+				tileMap.set_cell(0,i+Vector2i(x,y),0,forestTile)
+				for j in neighFour:
+					if tileMap.get_cell_atlas_coords(0, i+Vector2i(x,y)+j) == Vector2i(-1,-1):
+						tileMap.set_cell(0,i+Vector2i(x,y)+j,0,wallTile)
+		
+	print("avoidance running")
+	for i in pathWayCoords:
+		var points = walker(i, gatewayCoords)
 		for j in points:
 			tileMap.set_cell(0,j,0,forestTile)
+			#this is part two of not greatness to block the player in
+			for k in neighFour:
+				if tileMap.get_cell_atlas_coords(0, j+Vector2(k)) == Vector2i(-1,-1):
+					tileMap.set_cell(0,j+Vector2(k),0,wallTile)
+					
+func generate_gateways(amount):
+	var _gateways = []
 	
-	#now the rooms, again just gonna be 15x15 tile squares for this example
-	for i in rooms:
-		for x in range(-7, 7):
-			for y in range(-7, 7):
-				tileMap.set_cell(0,i+Vector2i(x,y),0,forestTile)
-		tileMap.set_cell(0,i,0,doorWayTile)
+	var grid = []
+	for x in amount*gridScaleFactor:
+		grid.append([])
+		for y in amount*gridScaleFactor:
+			grid[x].append(0)
+	var gridSize = grid.size()
+			
+	while _gateways.size() < amount:
+		var gatewayCentre = Vector2i(randi_range(0, gridSize-1),randi_range(0, gridSize-1))
 		
+		if !grid[gatewayCentre.x][gatewayCentre.y]:
+			grid[gatewayCentre.x][gatewayCentre.y] = 1
+			for i in neighEight:
+				var neighbour = gatewayCentre + i
+				if neighbour.x in range(0, gridSize) and neighbour.y in range(0, gridSize):
+					grid[neighbour.x][neighbour.y] = 1
+					
+			var gateway = gatewayScenes.pick_random().instantiate()
+			gateway.position = (gatewayCentre * gridCellSize) - (Vector2i(1,1) * (gridCellSize*gridSize/2))
+			_gateways.append(gateway)
+	return _gateways
+	
 func find_mst(points):
 	var aStarPath = AStar2D.new()
 	aStarPath.add_point(aStarPath.get_available_point_id(), points.pop_front())
@@ -117,43 +167,85 @@ func find_mst(points):
 	
 	return pathVectors
 
-func walker(points):
+func find_tri_edges(points):
+	var pointsTri = Geometry2D.triangulate_delaunay(points)
+	var pathVectors = []
+	for i in range(0, pointsTri.size(), 3):
+		pathVectors.append([points[pointsTri[i]],
+							points[pointsTri[i + 1]]])
+		pathVectors.append([points[pointsTri[i + 1]],
+							points[pointsTri[i + 2]]])
+		pathVectors.append([points[pointsTri[i + 2]],
+							points[pointsTri[i]]])
+	return pathVectors
+
+func walker(points, obstacles):
+	print("avoidance running")
 	var start = Vector2(points[0])
 	var end = Vector2(points[1])
 	var walkerPoints = []
+#	obstacles.erase(obstacles.find(start))
 	
 	while start != end:
 		var weights = []
 		var totalWeight = 0
 		
+		var nearestObstacle = null
+		var minDist = INF
+		for i in obstacles:
+			if start.distance_to(i) < minDist:
+				minDist = start.distance_to(i)
+				nearestObstacle = i
+				
 		for i in neighFour:
-			var weight = (start+Vector2(i)).distance_to(end)
-			weights.append((1/exp(weight)))
-			totalWeight += (1/exp(weight))
+			var weight = 0
+			if (start+Vector2(i)).distance_to(nearestObstacle) > pathwayAvoidanceDist:
+				weight = (start+Vector2(i)).distance_to(end)
+				weights.append((1/exp(weight)))
+				totalWeight += (1/exp(weight))
 		
-		#normalize weights
+		if !totalWeight:
+			weights.clear()
+			weights = []
+			totalWeight = 0
+			for i in neighFour:
+				var weight = (start+Vector2(i)).distance_to(end)
+				weights.append((1/exp(weight)))
+				totalWeight += (1/exp(weight))
+			
 		for i in weights.size():
-			weights[i] = (weights[i]/totalWeight)
-		# Generate a random number between 0 and the total weight
+			if weights[i] != 0:
+				weights[i] = (weights[i]/totalWeight)
+		
 		var randomValue = randf()
-		# Determine the chosen option based on the weights
 		var cumulativeWeight = 0.0
 		var direction = Vector2.ZERO
 		for i in range(weights.size()):
+			
 			cumulativeWeight += weights[i]
-			if randomValue < cumulativeWeight:
-				direction = Vector2(neighFour[i])
-				break
+			if weights[i] != 0:
+				if randomValue < cumulativeWeight:
+					direction = Vector2(neighFour[i])
+					break
 				
 		start += direction
-		#this part is messy, but it is what defines the width of the pathways, ideally
-		#this can be randomized too.
+		
+		var rWidth = randi_range(2,4)
+		var rHeight = randi_range(2,4)
 		walkerPoints.append(start)
-		walkerPoints.append(start+Vector2(1,0))
-		walkerPoints.append(start+Vector2(0,1))
-		walkerPoints.append(start+Vector2(1,1))
+		for x in range(-rWidth, rWidth):
+			for y in range(-rHeight, rHeight):
+				walkerPoints.append(start+Vector2(x,y))
 	
 	return walkerPoints
-
+	
 func _process(delta):
-	pass
+	if is_mapPreview:
+		if Input.is_action_just_released("ui_select"):
+			is_mapPreview = false
+			var player = playerScene.instantiate()
+			player.position = gateways.pick_random().position
+			$Camera2D.enabled = false
+			$CanvasLayer.visible = false
+			player.get_node("Camera2D").enabled = true
+			add_child(player)
