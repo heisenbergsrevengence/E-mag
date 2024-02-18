9d2799ecf8c945416abaa8dc10d996213e46ece3
SkotDude <159684904+SkotDude@users.noreply.github.com>
2024-02-19 02:40:39 +1000

    fixing mess

--- godot/hubgentest/mapgen.gd
+++ godot/hubgentest/mapgen.gd
@@ -1,22 +1,51 @@
 extends Node2D
 
-@onready var tileMap = $TileMap
+#Debug settings
+enum gameModes		{DEBUGMAPSETUP,
+					DEBUGMAPGEN,
+					PLAY
+					}
+@export var isDebug: bool = true
+@export var defaultGameMode: int = gameModes.DEBUGMAPSETUP
+var isAnimationSkipped: bool = false
+var isSkipAllAnimations: bool = false
+var debugLog: PackedStringArray = []
 
-var gatewayScenes = [preload("res://gateways/gateway0.tscn"), preload("res://gateways/gateway1.tscn")]
+#Camera and window settings
+@export var screenTransitionMargin: Vector2 = Vector2(8,8)
+@export var playerCameraZoom: Vector2 = Vector2(4,4)
+var screenTransitionPoint: Vector2
+var cameraMode: int = defaultGameMode
+@onready var camera = $Camera
+
+#Map Generation Settings:
+@export var mapSeed: int = 69420
+@export var gatewayAmount: int = 15
+@export var attemptsPerGateway: int = 20
+@export var gridScaleFactor: float = 0.55
+@export var gridCellSize: int = 224
+@export var extraPathwayFactor: float = 0.1
+@export var gatewayScenePath: String = "res://gateways/"
+@onready var tileMap = $TileMap
+var floorSource: int = 1
+var blackSource: int = 2
+var wallSource: int = 4
+var floorTile: Vector2i = Vector2i(0,0)
+var blackTile: Vector2i = Vector2i(2,1)
+var wallTile: Vector2i = Vector2i(0,0)
+
+var gatewayScenes: Array
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var rngState: int = 0
+var tileMapCellSize = 16
+
 var playerScene = preload("res://player.tscn")
 
-var floorTile = Vector2i(0,0)
-var blackTile = Vector2i(2,1)
-var wallTile = Vector2i(1,0)
-
-var neighFour = [Vector2i(1,0), Vector2i(0,1), Vector2i(-1,0), Vector2i(0,-1)]
-var neighEight = [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1), Vector2i(-1, 1), 
+var neighFour: Array = [Vector2i(1,0), Vector2i(0,1), Vector2i(-1,0), Vector2i(0,-1)]
+var neighEight: Array = [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1), Vector2i(-1, 1), 
 				Vector2i(-1, 0), Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1)]
 
-@export var gatewayAmount = 15
-@export var gridScaleFactor = 0.55
-@export var gridCellSize = 256-32
-@export var extraPathwayFactor = 0.1
+
 
 #these two variables affect the avoidance process... changing them can cause the map to load
 #very fast, but have unreachable areas, or take a very long/infinite time to load
@@ -41,8 +70,203 @@ var pointDraw = []
 var connectorsDraw = []
 
 func _ready():
-	screenBuffer = get_viewport_rect().size/4
-	print(screenBuffer)
+	get_tree().get_root().size_changed.connect(_on_screen_resize)
+	camera_setup()
+	gatewayScenes = gateway_scene_load(gatewayScenePath)
+	
+	#Generate map
+	await generate_map()
+	
+func generate_gateways(amount : int) -> Array:
+	var attempts: int = 0
+	var maxAttempts: int = amount * attemptsPerGateway
+	var _gateways: Array = []
+	var grid: Array = []
+	for x in amount*gridScaleFactor:
+		grid.append([])
+		for y in amount*gridScaleFactor:
+			grid[x].append(0)
+	var gridSize: int = grid.size()
+			
+	while _gateways.size() < amount:
+		attempts += 1
+		if attempts > maxAttempts:
+			debugLog.append("Failed to place " + str(amount) + " after " + str(attempts) + " attempts. Consider increasing grid scale or cell size.")
+			break
+		var gatewayCentre: Vector2i = Vector2i(rng.randi_range(0, gridSize-1),rng.randi_range(0, gridSize-1))
+		
+		if !grid[gatewayCentre.x][gatewayCentre.y]:
+			grid[gatewayCentre.x][gatewayCentre.y] = 1
+			for i in neighEight:
+				var neighbour: Vector2i = gatewayCentre + i
+				if neighbour.x in range(0, gridSize) and neighbour.y in range(0, gridSize):
+					grid[neighbour.x][neighbour.y] = 1
+			
+			gatewayCentre = (gatewayCentre * gridCellSize) - (Vector2i(1,1) * (gridCellSize*gridSize/2))
+			
+			var gateway: Node2D = gatewayScenes[rng.randi_range(0, gatewayScenes.size()-1)].instantiate()
+			var gatewayTileMap: TileMap = gateway.get_node("TileMap")
+			var gatewayTiles: Array = gatewayTileMap.get_used_cells(0)
+			
+			for i in gatewayTiles:
+				var atlasCoord: Vector2i = gatewayTileMap.get_cell_atlas_coords(0, i)
+				var sourceID: int = gatewayTileMap.get_cell_source_id(0, i)
+				tileMap.set_cell(0, tileMap.local_to_map(gatewayCentre)+i, sourceID, atlasCoord)
+				
+			gateway.position = gatewayCentre 
+			_gateways.append(gateway)
+			if isDebug and !isAnimationSkipped and !isSkipAllAnimations:
+				await get_tree().create_timer(0.01).timeout
+	return _gateways
+	
+func find_mst(points):
+	var aStarPath = AStar2D.new()
+	aStarPath.add_point(aStarPath.get_available_point_id(), points.pop_front())
+	while points:
+		var minDist = INF
+		var minPos = null
+		var pos = null
+		
+		for i in aStarPath.get_point_ids():
+			var pos1 = aStarPath.get_point_position(i)
+			
+			for pos2 in points:
+				if pos1.distance_to(pos2) < minDist:
+					minDist = pos1.distance_to(pos2)
+					minPos = pos2
+					pos = pos1
+					
+		var id = aStarPath.get_available_point_id()
+		aStarPath.add_point(id, minPos)
+		aStarPath.connect_points(aStarPath.get_closest_point(pos), id)
+		points.erase(minPos)
+	
+	var pathVectors = []
+	for i in aStarPath.get_point_ids():
+			for c in aStarPath.get_point_connections(i):
+				pathVectors.append(	[aStarPath.get_point_position(i), 
+									aStarPath.get_point_position(c)])
+	
+	return pathVectors
+
+func find_tri_edges(points):
+	var pointsTri = Geometry2D.triangulate_delaunay(points)
+	var pathVectors = []
+	for i in range(0, pointsTri.size(), 3):
+		pathVectors.append([points[pointsTri[i]],
+							points[pointsTri[i + 1]]])
+		pathVectors.append([points[pointsTri[i + 1]],
+							points[pointsTri[i + 2]]])
+		pathVectors.append([points[pointsTri[i + 2]],
+							points[pointsTri[i]]])
+	return pathVectors
+
+func walker(points, obstacles):
+#	print("avoidance running")
+	var start = Vector2(points[0])
+	var end = Vector2(points[1])
+	var walkerPoints = []
+#	print(start, ", ", end)
+#	obstacles.erase(obstacles.find(start))
+	
+	while start != end:
+		var weights = []
+		var totalWeight = 0
+		
+		var nearestObstacle = null
+		
+		if obstacles:
+			var minDist = INF
+			for i in obstacles:
+				if start.distance_to(i) < minDist:
+					minDist = start.distance_to(i)
+					nearestObstacle = i
+					
+			for i in neighFour:
+				var weight = 0
+				if (start+Vector2(i)).distance_to(nearestObstacle) > pathwayAvoidanceDist:
+					weight = (start+Vector2(i)).distance_to(end)
+					weights.append((1/exp(weight)))
+					totalWeight += (1/exp(weight))
+				else:
+					weight = (start+Vector2(i)).distance_to(nearestObstacle*-1)
+					weights.append((1/exp(weight)))
+					totalWeight += (1/exp(weight))
+			
+			if !totalWeight:
+				weights.clear()
+				weights = []
+				totalWeight = 0
+				for i in neighFour:
+					var weight = (start+Vector2(i)).distance_to(end)
+					weights.append((1/exp(weight)))
+					totalWeight += (1/exp(weight))
+		else:
+			for i in neighFour:
+					var weight = (start+Vector2(i)).distance_to(end)
+					weights.append((1/exp(weight)))
+					totalWeight += (1/exp(weight))
+			
+		for i in weights.size():
+			if weights[i] != 0:
+				weights[i] = (weights[i]/totalWeight)
+		
+		var randomValue = randf()
+		var cumulativeWeight = 0.0
+		var direction = Vector2.RIGHT
+		for i in weights.size():
+			cumulativeWeight += weights[i]
+			if weights[i] != 0:
+				if randomValue < cumulativeWeight:
+					direction = Vector2(neighFour[i])
+					break
+				
+		start += direction
+		
+		var rWidth = randi_range(2,4)
+		var rHeight = randi_range(2,4)
+		walkerPoints.append(start)
+		for x in range(-rWidth, rWidth):
+			for y in range(-rHeight, rHeight):
+				walkerPoints.append(start+Vector2(x,y))
+#	print("finished")
+	return walkerPoints
+	
+func _process(delta):
+	if is_mapPreview:
+		if Input.is_action_just_released("ui_select"):
+			is_mapPreview = false
+			player = playerScene.instantiate()
+			player.position = gateways.pick_random().position
+			$Camera2D.global_position = player.global_position
+			cameraTarget = $Camera2D.global_position
+			$CanvasLayer.visible = false
+			$Camera2D.zoom = Vector2(4,4)
+			add_child(player)
+	else:
+		if Input.is_action_just_released("ui_accept"):
+			instantCamera = !instantCamera
+		var playerOffset = (cameraTarget - player.global_position)
+		if playerOffset.x > screenBuffer.x/2 -16 and Input.is_action_pressed("ui_left"):
+			cameraTarget.x = cameraTarget.x - screenBuffer.x
+		if playerOffset.x < -screenBuffer.x/2 + 16 and Input.is_action_pressed("ui_right"):
+			cameraTarget.x = cameraTarget.x + screenBuffer.x
+		if playerOffset.y > screenBuffer.y/2 - 16 and Input.is_action_pressed("ui_up"):
+			cameraTarget.y = cameraTarget.y - screenBuffer.y
+		if playerOffset.y < -screenBuffer.y/2 + 16 and Input.is_action_pressed("ui_down"):
+			cameraTarget.y = cameraTarget.y + screenBuffer.y
+		if $Camera2D.global_position != cameraTarget:
+			if instantCamera:
+				$Camera2D.global_position = cameraTarget
+			else:
+				$Camera2D.global_position = $Camera2D.global_position.move_toward(cameraTarget, 32)
+
+func generate_map() -> void:
+	if !gatewayScenes.size():
+		debugLog.append("Failed to load any gateway scenes. Aborting map generation.")
+		return
+	
+	rng.seed = mapSeed
 	
 	var gatewayCoords = []
 	var connections = []
@@ -53,16 +277,26 @@ func _ready():
 	var gatewayInnerRect = {}
 	var gatewayConnectors = []
 	
-	gateways = generate_gateways(gatewayAmount)
+	debugLog.append("Starting map generation...")
 	
+	if !isSkipAllAnimations:
+		debugLog.append("Press space to skip the current animation, press enter to skip all. This does not affect generation, only animation.")
+	debugLog.append("Placing " + str(gatewayAmount) + "gateways... ")
+	
+	gateways = await generate_gateways(gatewayAmount)
+	
+	if gateways.size() < gatewayAmount:
+		debugLog.append("Failed to place all gateways. Aborting map generation.")
+		return
+		
 	for i in gateways:
 		gatewayCoords.append(tileMap.local_to_map(i.position))
 
 		var gateway = i
 		var gatewayTileMap = gateway.get_node("TileMap")
 		var size = Vector2(gatewayTileMap.get_used_rect().size)
-		size = size * 16 #tile size
-		var innerSize = Vector2(gatewayTileMap.get_used_rect().size) * 16
+		size = size * tileMapCellSize #tile size
+		var innerSize = Vector2(gatewayTileMap.get_used_rect().size) * tileMapCellSize
 		size.x += entryWayDist
 		size.y += entryWayDist
 		var gatewayPos = gateway.global_position + gatewayTileMap.map_to_local(gatewayTileMap.get_used_rect().position) - Vector2(8,8) - Vector2(entryWayDist/2, entryWayDist/2)
@@ -237,13 +471,13 @@ func _ready():
 		if tileMap.get_cell_tile_data(0, i).get_custom_data("floor"):
 			for j in neighFour:
 				if tileMap.get_cell_atlas_coords(0, i+j) == Vector2i(-1,-1):
-					tileMap.set_cell(0,i+j,0,wallTile)
+					tileMap.set_cell(0,i+j,4,wallTile)
 
-	for i in tileMap.get_used_cells(0):
-#		if tileMap.get_cell_tile_data(0, i).get_custom_data("floor"):
-		for j in neighFour:
-			if tileMap.get_cell_atlas_coords(0, i+j) == Vector2i(-1,-1):
-				tileMap.set_cell(0,i+j,0,wallTile)
+#	for i in tileMap.get_used_cells(0):
+##		if tileMap.get_cell_tile_data(0, i).get_custom_data("floor"):
+#		for j in neighFour:
+#			if tileMap.get_cell_atlas_coords(0, i+j) == Vector2i(-1,-1):
+#				tileMap.set_cell(0,i+j,4,wallTile)
 
 	var tileMapSize = tileMap.get_used_rect().size + Vector2i(30,30)
 	var tileMapPos = tileMap.get_used_rect().position - Vector2i(15,15)
@@ -254,7 +488,6 @@ func _ready():
 	
 	BetterTerrain.update_terrain_cells(tileMap, 0, tileMap.get_used_cells(0), true)
 	
-#	BetterTerrain.set_cells(tileMap, 1, unusedCells, 0) 
 #func _draw():
 #	if pathDraw:
 #		for i in pathDraw:
@@ -271,179 +504,39 @@ func _ready():
 #	if pointDraw:
 #		for i in pointDraw:
 #			draw_circle(i, 5,Color.RED)
-	print(get_viewport_rect())
-func generate_gateways(amount):
-	var _gateways = []
 	
-	var grid = []
-	for x in amount*gridScaleFactor:
-		grid.append([])
-		for y in amount*gridScaleFactor:
-			grid[x].append(0)
-	var gridSize = grid.size()
-			
-	while _gateways.size() < amount:
-		var gatewayCentre = Vector2i(randi_range(0, gridSize-1),randi_range(0, gridSize-1))
-		
-		if !grid[gatewayCentre.x][gatewayCentre.y]:
-			grid[gatewayCentre.x][gatewayCentre.y] = 1
-			for i in neighEight:
-				var neighbour = gatewayCentre + i
-				if neighbour.x in range(0, gridSize) and neighbour.y in range(0, gridSize):
-					grid[neighbour.x][neighbour.y] = 1
-			
-			gatewayCentre = (gatewayCentre * gridCellSize) - (Vector2i(1,1) * (gridCellSize*gridSize/2))
-			
-			var gateway = gatewayScenes.pick_random().instantiate()
-			var gatewayTileMap = gateway.get_node("TileMap")
-			var gatewayTiles = gatewayTileMap.get_used_cells(0)
-			
-			for i in gatewayTiles:
-				var atlasCoord = gatewayTileMap.get_cell_atlas_coords(0, i)
-				var sourceID = gatewayTileMap.get_cell_source_id(0, i)
-				tileMap.set_cell(0, tileMap.local_to_map(gatewayCentre)+i, sourceID, atlasCoord)
-			gateway.position = gatewayCentre 
-			_gateways.append(gateway)
-	return _gateways
-	
-func find_mst(points):
-	var aStarPath = AStar2D.new()
-	aStarPath.add_point(aStarPath.get_available_point_id(), points.pop_front())
-	while points:
-		var minDist = INF
-		var minPos = null
-		var pos = null
-		
-		for i in aStarPath.get_point_ids():
-			var pos1 = aStarPath.get_point_position(i)
-			
-			for pos2 in points:
-				if pos1.distance_to(pos2) < minDist:
-					minDist = pos1.distance_to(pos2)
-					minPos = pos2
-					pos = pos1
-					
-		var id = aStarPath.get_available_point_id()
-		aStarPath.add_point(id, minPos)
-		aStarPath.connect_points(aStarPath.get_closest_point(pos), id)
-		points.erase(minPos)
-	
-	var pathVectors = []
-	for i in aStarPath.get_point_ids():
-			for c in aStarPath.get_point_connections(i):
-				pathVectors.append(	[aStarPath.get_point_position(i), 
-									aStarPath.get_point_position(c)])
-	
-	return pathVectors
-
-func find_tri_edges(points):
-	var pointsTri = Geometry2D.triangulate_delaunay(points)
-	var pathVectors = []
-	for i in range(0, pointsTri.size(), 3):
-		pathVectors.append([points[pointsTri[i]],
-							points[pointsTri[i + 1]]])
-		pathVectors.append([points[pointsTri[i + 1]],
-							points[pointsTri[i + 2]]])
-		pathVectors.append([points[pointsTri[i + 2]],
-							points[pointsTri[i]]])
-	return pathVectors
-
-func walker(points, obstacles):
-#	print("avoidance running")
-	var start = Vector2(points[0])
-	var end = Vector2(points[1])
-	var walkerPoints = []
-#	print(start, ", ", end)
-#	obstacles.erase(obstacles.find(start))
-	
-	while start != end:
-		var weights = []
-		var totalWeight = 0
-		
-		var nearestObstacle = null
-		
-		if obstacles:
-			var minDist = INF
-			for i in obstacles:
-				if start.distance_to(i) < minDist:
-					minDist = start.distance_to(i)
-					nearestObstacle = i
-					
-			for i in neighFour:
-				var weight = 0
-				if (start+Vector2(i)).distance_to(nearestObstacle) > pathwayAvoidanceDist:
-					weight = (start+Vector2(i)).distance_to(end)
-					weights.append((1/exp(weight)))
-					totalWeight += (1/exp(weight))
+func gateway_scene_load(path: String) -> Array:
+	var _gatewayScenes: Array = []
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file: String = dir.get_next()
+		while file != "":
+			if file.get_extension() == "tscn":
+				var _gatewayScene: PackedScene = load(path + file)
+				var sceneState: SceneState = _gatewayScene.get_state()
+				if sceneState.get_node_name(1) != "TileMap":
+					debugLog.append(str(path+file) + " was not loaded: Improper configuration/no tile map.")
+				elif sceneState.get_node_name(2) != "entry_points":
+					debugLog.append(str(path+file) + " was not loaded: Improper configuration/no entry points.")
 				else:
-					weight = (start+Vector2(i)).distance_to(nearestObstacle*-1)
-					weights.append((1/exp(weight)))
-					totalWeight += (1/exp(weight))
-			
-			if !totalWeight:
-				weights.clear()
-				weights = []
-				totalWeight = 0
-				for i in neighFour:
-					var weight = (start+Vector2(i)).distance_to(end)
-					weights.append((1/exp(weight)))
-					totalWeight += (1/exp(weight))
-		else:
-			for i in neighFour:
-					var weight = (start+Vector2(i)).distance_to(end)
-					weights.append((1/exp(weight)))
-					totalWeight += (1/exp(weight))
-			
-		for i in weights.size():
-			if weights[i] != 0:
-				weights[i] = (weights[i]/totalWeight)
-		
-		var randomValue = randf()
-		var cumulativeWeight = 0.0
-		var direction = Vector2.RIGHT
-		for i in weights.size():
-			cumulativeWeight += weights[i]
-			if weights[i] != 0:
-				if randomValue < cumulativeWeight:
-					direction = Vector2(neighFour[i])
-					break
-				
-		start += direction
-		
-		var rWidth = randi_range(2,4)
-		var rHeight = randi_range(2,4)
-		walkerPoints.append(start)
-		for x in range(-rWidth, rWidth):
-			for y in range(-rHeight, rHeight):
-				walkerPoints.append(start+Vector2(x,y))
-#	print("finished")
-	return walkerPoints
-	
-func _process(delta):
-	if is_mapPreview:
-		if Input.is_action_just_released("ui_select"):
-			is_mapPreview = false
-			player = playerScene.instantiate()
-			player.position = gateways.pick_random().position
-			$Camera2D.global_position = player.global_position
-			cameraTarget = $Camera2D.global_position
-			$CanvasLayer.visible = false
-			$Camera2D.zoom = Vector2(4,4)
-			add_child(player)
+					_gatewayScenes.append(_gatewayScene)
+			file = dir.get_next()
 	else:
-		if Input.is_action_just_released("ui_accept"):
-			instantCamera = !instantCamera
-		var playerOffset = (cameraTarget - player.global_position)
-		if playerOffset.x > screenBuffer.x/2 -16 and Input.is_action_pressed("ui_left"):
-			cameraTarget.x = cameraTarget.x - screenBuffer.x
-		if playerOffset.x < -screenBuffer.x/2 + 16 and Input.is_action_pressed("ui_right"):
-			cameraTarget.x = cameraTarget.x + screenBuffer.x
-		if playerOffset.y > screenBuffer.y/2 - 16 and Input.is_action_pressed("ui_up"):
-			cameraTarget.y = cameraTarget.y - screenBuffer.y
-		if playerOffset.y < -screenBuffer.y/2 + 16 and Input.is_action_pressed("ui_down"):
-			cameraTarget.y = cameraTarget.y + screenBuffer.y
-		if $Camera2D.global_position != cameraTarget:
-			if instantCamera:
-				$Camera2D.global_position = cameraTarget
-			else:
-				$Camera2D.global_position = $Camera2D.global_position.move_toward(cameraTarget, 32)
+		debugLog.append("Error loading the gateway directory. Supplied path does not exist.")
+		
+	if !_gatewayScenes.size():
+		debugLog.append("No scenes found in the gateway directory. Map generation will not proceed.")
+	
+	return _gatewayScenes
+
+func camera_setup() -> void:
+	set_screen_transition_point()
+	if !isDebug:
+		camera.zoom = playerCameraZoom
+
+func set_screen_transition_point() -> void:
+	screenTransitionPoint = (get_viewport_rect().size/playerCameraZoom) - screenTransitionMargin
+	
+func _on_screen_resize() -> void:
+	set_screen_transition_point()
--- godot/hubgentest/mathtest.gd
+++ godot/hubgentest/mathtest.gd
@@ -6,73 +6,79 @@ var pathPointDraw = []
 var entryDraw = []
 var connectorsDraw = []
 
+var isAnimationSkipped = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var gatewayScene = preload("res://gateways/gateway1.tscn")
-	var gateway = gatewayScene.instantiate()
-	gateway.position = Vector2(600,600)
-	add_child(gateway)
-	var entrywaydist = 128
-	var gatewayTileMap = gateway.get_node("TileMap")
-	var size = Vector2(gateway.get_node("TileMap").get_used_rect().size)
-	
-	size = size * 16#tile size
-	size.x += entrywaydist
-	size.y += entrywaydist
-	
-	var gatewayPos = gateway.global_position + gatewayTileMap.map_to_local(gatewayTileMap.get_used_rect().position) - Vector2(8,8) - Vector2(entrywaydist/2, entrywaydist/2)
-		
-	var gatewayCorners = [gatewayPos, Vector2(gatewayPos.x + size.x, gatewayPos.y), gatewayPos + size, Vector2(gatewayPos.x, gatewayPos.y+size.y)]
-	var gatewayRect = []
-	for i in gatewayCorners.size():
-		gatewayRect.append([gatewayCorners[i], gatewayCorners[(i+1) % gatewayCorners.size()]])
-	
-	var entryPoints = []
-	var gatewayConnectors = []
-	for i in gateway.get_node("entry_points").get_children():
-		var entryLine = i.global_position + (Vector2.RIGHT.rotated(gateway.global_position.angle_to_point(i.global_position))*entrywaydist*1.75)
-		for j in gatewayRect:
-			var entryPoint = Geometry2D.segment_intersects_segment(gateway.global_position, entryLine, j[0], j[1])
-			if entryPoint:
-				entryPoints.append(round(entryPoint))
-				gatewayConnectors.append([gatewayPos, entryPoint])
-				connectorsDraw.append([i.global_position, entryPoint])
-	
-	var gatewayPaths = []
-	
-	var usedPoints = []
-	for i in gatewayCorners.size():
-		var start = gatewayCorners[i]
-		var end = gatewayCorners[(i+1) % gatewayCorners.size()]
-		while start != end:
-			pathPointDraw.append(start)
-			var is_intercepted = false
-			for j in entryPoints:
-				if !(j in usedPoints):
-#					var line_vector = end - start
-#					var point_vector = j - start
-#					var dot_product = point_vector.dot(line_vector)
-#					var line_length_squared = line_vector.length_squared()
-#					var point_distance_squared = point_vector.length_squared()
-#					if dot_product >= 0 and dot_product <= line_length_squared and point_distance_squared <= line_length_squared:
-					if start.distance_to(j)+end.distance_to(j) == start.distance_to(end):
-						gatewayPaths.append([start, j])
-						pathDraw1.append([start, j])
-						start = j
-						is_intercepted = true
-						usedPoints.append(j)
-						break
-						
-			if !is_intercepted:
-				gatewayPaths.append([start, end])
-				pathDraw1.append([start, end])
-				start = end
-
-	print(gatewayPaths)
-	for i in gatewayRect:
-		pathDraw.append([i[0],i[1]])
-	
-	entryDraw = entryPoints.duplicate()
+	for i in 10:
+		print(i)
+		if !isAnimationSkipped:
+			await get_tree().create_timer(0.0001).timeout
+	isAnimationSkipped = false
+#	var gatewayScene = preload("res://gateways/gateway1.tscn")
+#	var gateway = gatewayScene.instantiate()
+#	gateway.position = Vector2(600,600)
+#	add_child(gateway)
+#	var entrywaydist = 128
+#	var gatewayTileMap = gateway.get_node("TileMap")
+#	var size = Vector2(gateway.get_node("TileMap").get_used_rect().size)
+#
+#	size = size * 16#tile size
+#	size.x += entrywaydist
+#	size.y += entrywaydist
+#
+#	var gatewayPos = gateway.global_position + gatewayTileMap.map_to_local(gatewayTileMap.get_used_rect().position) - Vector2(8,8) - Vector2(entrywaydist/2, entrywaydist/2)
+#
+#	var gatewayCorners = [gatewayPos, Vector2(gatewayPos.x + size.x, gatewayPos.y), gatewayPos + size, Vector2(gatewayPos.x, gatewayPos.y+size.y)]
+#	var gatewayRect = []
+#	for i in gatewayCorners.size():
+#		gatewayRect.append([gatewayCorners[i], gatewayCorners[(i+1) % gatewayCorners.size()]])
+#
+#	var entryPoints = []
+#	var gatewayConnectors = []
+#	for i in gateway.get_node("entry_points").get_children():
+#		var entryLine = i.global_position + (Vector2.RIGHT.rotated(gateway.global_position.angle_to_point(i.global_position))*entrywaydist*1.75)
+#		for j in gatewayRect:
+#			var entryPoint = Geometry2D.segment_intersects_segment(gateway.global_position, entryLine, j[0], j[1])
+#			if entryPoint:
+#				entryPoints.append(round(entryPoint))
+#				gatewayConnectors.append([gatewayPos, entryPoint])
+#				connectorsDraw.append([i.global_position, entryPoint])
+#
+#	var gatewayPaths = []
+#
+#	var usedPoints = []
+#	for i in gatewayCorners.size():
+#		var start = gatewayCorners[i]
+#		var end = gatewayCorners[(i+1) % gatewayCorners.size()]
+#		while start != end:
+#			pathPointDraw.append(start)
+#			var is_intercepted = false
+#			for j in entryPoints:
+#				if !(j in usedPoints):
+##					var line_vector = end - start
+##					var point_vector = j - start
+##					var dot_product = point_vector.dot(line_vector)
+##					var line_length_squared = line_vector.length_squared()
+##					var point_distance_squared = point_vector.length_squared()
+##					if dot_product >= 0 and dot_product <= line_length_squared and point_distance_squared <= line_length_squared:
+#					if start.distance_to(j)+end.distance_to(j) == start.distance_to(end):
+#						gatewayPaths.append([start, j])
+#						pathDraw1.append([start, j])
+#						start = j
+#						is_intercepted = true
+#						usedPoints.append(j)
+#						break
+#
+#			if !is_intercepted:
+#				gatewayPaths.append([start, end])
+#				pathDraw1.append([start, end])
+#				start = end
+#
+#	print(gatewayPaths)
+#	for i in gatewayRect:
+#		pathDraw.append([i[0],i[1]])
+#
+#	entryDraw = entryPoints.duplicate()
 
 func _draw():
 	if pathDraw:
@@ -94,4 +100,7 @@ func _draw():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_select"):
+		isAnimationSkipped = true
+		print("skipped")
 	pass
